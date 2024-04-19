f542e9583577092d87358089978ababfe05cb9ca
RoseSkullIXVI <athosjnr@gmail.com>
2024-04-19 20:36:59 +0300

    Final(ish)

--- src/main/java/com/dynamicapi/api_dynamic/Hadoop/HadoopService.java
+++ src/main/java/com/dynamicapi/api_dynamic/Hadoop/HadoopService.java
@@ -15,9 +15,6 @@ import java.util.Date;
 public class HadoopService {
 
     private FileSystem fileSystem;
-    private static final int MAX_RETRIES = 3;  // Maximum number of retries
-    private static final long RETRY_WAIT_TIME = 3000;  // Wait time between retries in milliseconds
-
     public HadoopService(FileSystem fileSystem) {
         this.fileSystem = fileSystem;
     }
@@ -25,26 +22,23 @@ public class HadoopService {
     public void appendJsonStringToHdfsFile(String jsonString, String filename) throws IOException {
         Path path = new Path("hdfs://roseskull:8020/blueprint/input/"+filename+"_blueprint.json");
         Path parentDir = path.getParent();
+        FSDataOutputStream output = null;
 
         if (!fileSystem.exists(parentDir)) {
             fileSystem.mkdirs(parentDir);
             System.out.println("Directory created: " + parentDir);
         }
-
-        if (!fileSystem.exists(path)) {
-            try (FSDataOutputStream output = fileSystem.create(path)) {
-                output.writeChars(jsonString);
-                System.out.println("File created");
-                output.close();
-            } catch (Exception e) {
-                System.out.println("Error creating file: " + e.getMessage());
-            }
-        } else {
-            appendWithRetry(path, jsonString);
+        try {
+            output = fileSystem.create(path);
+            output.writeChars(jsonString);
+            System.out.println("File created");
+            output.close();
+        } catch (Exception e) {
+            System.out.println("Error creating file: " + e.getMessage());
         }
     }
     public void insertInputStream (InputStream input, String filename) throws IOException{
-        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String timeStamp = new SimpleDateFormat("ddMMyyyy").format(new Date());
         Path path = new Path("hdfs://roseskull:8020/blueprint/input/"+timeStamp+"_"+filename);
         Path parentDir = path.getParent();
 
@@ -52,42 +46,44 @@ public class HadoopService {
             fileSystem.mkdirs(parentDir);
             System.out.println("Directory created: " + parentDir);
         }
-
-        if (!fileSystem.exists(path)) {
+        try{
             FSDataOutputStream output = fileSystem.create(path);
             IOUtils.copyBytes(input, output, fileSystem.getConf());  
             System.out.println("File writen to HDFS"); 
-        } else {
-            System.out.println("File already exists");
+        }catch (Exception e)
+        {
+            System.out.println("Error creating file: " + e.getMessage());
         }
+       
 
+       
         
     }
 
-    private void appendWithRetry(Path path, String jsonString) throws IOException {
-        int retryCount = 0;
-        boolean success = false;
-        while (retryCount < MAX_RETRIES && !success) {
-            try (FSDataOutputStream output = fileSystem.append(path)) {
-                output.writeChars(jsonString);
-                System.out.println("File appended");
-                success = true; 
-                output.close(); // Set success to true if append succeeds
-            } catch (IOException e) {
-                System.out.println("Attempt " + (retryCount + 1) + " failed: " + e.getMessage());
-                if (retryCount < MAX_RETRIES - 1) {
-                    try {
-                        Thread.sleep(RETRY_WAIT_TIME);  // Wait before retrying
-                    } catch (InterruptedException ie) {
-                        Thread.currentThread().interrupt();
-                        throw new IOException("Retry interrupted", ie);
-                    }
-                }
-                retryCount++;
-            }
-        }
-        if (!success) {
-            throw new IOException("Failed to append to file after " + MAX_RETRIES + " attempts");
-        }
-    }
+    // private void appendWithRetry(Path path, String jsonString) throws IOException {
+    //     int retryCount = 0;
+    //     boolean success = false;
+    //     while (retryCount < MAX_RETRIES && !success) {
+    //         try (FSDataOutputStream output = fileSystem.append(path)) {
+    //             output.writeChars(jsonString);
+    //             System.out.println("File appended");
+    //             success = true; 
+    //             output.close(); // Set success to true if append succeeds
+    //         } catch (IOException e) {
+    //             System.out.println("Attempt " + (retryCount + 1) + " failed: " + e.getMessage());
+    //             if (retryCount < MAX_RETRIES - 1) {
+    //                 try {
+    //                     Thread.sleep(RETRY_WAIT_TIME);  // Wait before retrying
+    //                 } catch (InterruptedException ie) {
+    //                     Thread.currentThread().interrupt();
+    //                     throw new IOException("Retry interrupted", ie);
+    //                 }
+    //             }
+    //             retryCount++;
+    //         }
+    //     }
+    //     if (!success) {
+    //         throw new IOException("Failed to append to file after " + MAX_RETRIES + " attempts");
+    //     }
+    // }
 }
